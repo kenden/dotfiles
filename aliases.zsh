5cade93eeff5cc6cb01d83453908daf46acf3bb9
Quentin Nerden <quentin.nerden@stylight.com>
2015-12-18 11:46:57 +0100

    add some aliases

---
+++ aliases.zsh
@@ -0,0 +1,23 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+#
+#
+alias ll='ls -Alrth'
+alias cds='cd ~/dev/stylight/'
+
+# git
+alias gs='git status'
+alias gpull='git pull'
+alias gpush='git push'
+alias gdiff='git diff'
+alias gcheck='git checkout'
+
+alias dex='docker exec -it'
+
+#export LC_CTYPE=en_US.UTF-8
+
+
+
