f154b48804a7fd662f98ceef7c3618d3372babe9
Quentin Nerden <quentin.nerden@stylight.com>
2016-07-14 09:51:29 +0200

    add aliases from st

--- aliases.zsh
+++ aliases.zsh
@@ -26,4 +26,13 @@ alias vi='vim'  # needed after installing vim with brew, else vi points to the o
 #export LC_CTYPE=en_US.UTF-8
 
 
+#local dev docker
+alias wfstop='docker-compose --file ~/dev/stylight/stylight-docker/bin/start_wildfly-docker-compose.yml stop'
+alias wfstart='docker-compose --file ~/dev/stylight/stylight-docker/bin/start_wildfly-docker-compose.yml up -d'
+alias wflog='docker exec -it wildfly less /srv/var/log/jboss/jboss.log'
+alias wf='docker-compose --file ~/dev/stylight/stylight-docker/bin/start_wildfly-docker-compose.yml'
+
+#puppet
+alias pullpuppet_de="ssh de-master.aws.stylight.net 'cd /etc/puppet/ && sudo git pull'"
+alias pullpuppet_eu="ssh eu-master.aws.stylight.net 'cd /etc/puppet/ && sudo git pull'"
 
